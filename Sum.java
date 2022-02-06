3db450a9d0aec792b3ca811c09e692cde3ac9329
Ankit Pal <ankitpal28090016@gmail.com>
2022-02-06 12:35:58 +0530

    added message in sum.java

--- Sum.java
+++ Sum.java
@@ -3,6 +3,6 @@ public class Sum{
     public static void main(String []args){
         int a=3;
         int b=7;
-        System.out.println(a+b);
+        System.out.println("The sum is " + (a+b));
     }
 }
